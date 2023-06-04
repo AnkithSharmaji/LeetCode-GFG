f9d0f9d05a331926fa76c16c9c94438d5cd08c62
Ankith Sharma <86291521+AnkithSharmaji@users.noreply.github.com>
2023-06-04 11:05:01 +0530

    Added solution - LeetHub

---
+++ Reversing the equation - GFG/reversing-the-equation.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        while(t-- > 0)
+        {
+            String s;
+            s = in.readLine().trim();
+            
+            Solution ob = new Solution();
+            
+            out.println(ob.reverseEqn(s));    
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+   
+    String reverseEqn(String S)
+    {
+        // your code here
+        StringBuilder reversed = new StringBuilder();
+        StringBuilder number = new StringBuilder();
+
+        for (int i = S.length() - 1; i >= 0; i--) {
+            char c = S.charAt(i);
+
+            if (Character.isDigit(c)) {
+                number.append(c);
+            } else {
+                reversed.append(number.reverse());
+                number.setLength(0);
+                reversed.append(c);
+            }
+        }
+
+        reversed.append(number.reverse());
+        return reversed.toString();
+    }
+}
